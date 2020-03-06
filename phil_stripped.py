929970ee86514eb752a4b140a7974b672fd066d0
RomSnow <ivashkin.roma2001@yandex.ru>
2020-03-06 22:15:22 +0500

    Творчество

--- phil_stripped.py
+++ phil_stripped.py
@@ -4,7 +4,7 @@ from urllib.parse import quote
 import re
 
 
-def get_content(name: str):
+def get_content(name: str) -> str:
     """
     Функция возвращает содержимое вики-страницы name из русской Википедии.
     В случае ошибки загрузки или отсутствия страницы возвращается None.
@@ -13,10 +13,10 @@ def get_content(name: str):
         link = name
     else:
         link = 'http://ru.wikipedia.org/wiki/' + quote(name)
-
     try:
         page = urlopen(link)
     except Exception:
+        print(name)
         return None
 
     return page.read().decode('utf=8')
@@ -53,13 +53,25 @@ def get_page_title(page: str) -> str:
     return str(*re.findall(r"<title>(.*)</title>", page))
 
 
-# def find_path(start_link: str, finish_link: str, path: list):
-#     path.append(start_link)
-#     if start_link == finish_link:
-#         return path
-#     get_content(start_link)
-#     links = extract_links(page, *extract_content(page))
-#     for i in links:
+def find_path(start_link: str, finish_link: str, path: list, counter: int):
+    if counter > 3:
+        return
+    print(counter)
+    print(start_link)
+    path.append(start_link)
+    if finish_link in start_link:
+        print('DONE')
+        return path
+    page = get_content(start_link)
+    if page is None:
+        return
+    links = extract_links(page, *extract_content(page))
+    counter += 1
+    for i in links:
+        if i in path:
+            continue
+        find_path(i, finish_link, path, counter)
+        
 
 
 def find_chain(start: str, finish: str) -> list:
@@ -73,10 +85,14 @@ def find_chain(start: str, finish: str) -> list:
 
 
 def main():
-    page = get_content('AWK')
+    page = get_content('Аристотель')
     begin, end = extract_content(page)
     links = extract_links(page, begin, end)
-    print(links)
+    print(len(links))
+    start_link = 'http://ru.wikipedia.org/wiki/' + quote('Аристотель')
+    finish_link = 'http://ru.wikipedia.org/wiki/' + quote('Философия')
+
+   # print(find_path(start_link, finish_link, [], 0))
 
 
 if __name__ == '__main__':
