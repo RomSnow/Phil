a3b7e9471c40b1df496c5f87e487ce7cbbc073d5
RomSnow <ivashkin.roma2001@yandex.ru>
2020-03-07 14:05:25 +0500

    Рабочий вариант

--- phil_stripped.py
+++ phil_stripped.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from urllib.request import urlopen
-from urllib.parse import quote
+from urllib.parse import quote, unquote
+from urllib.error import URLError, HTTPError
 import re
 
 
@@ -9,14 +10,10 @@ def get_content(name: str) -> str:
     Функция возвращает содержимое вики-страницы name из русской Википедии.
     В случае ошибки загрузки или отсутствия страницы возвращается None.
     """
-    if name.startswith('http'):
-        link = name
-    else:
-        link = 'http://ru.wikipedia.org/wiki/' + quote(name)
+    link = 'http://ru.wikipedia.org/wiki/' + quote(name)
     try:
         page = urlopen(link)
-    except Exception:
-        print(name)
+    except (URLError, HTTPError):
         return None
 
     return page.read().decode('utf=8')
@@ -30,7 +27,10 @@ def extract_content(page: str) -> tuple:
     содержимое статьи.
     Если содержимое отсутствует, возвращается (0, 0).
     """
-    return page.find('<div id="mw-content-text">'), page.find('</body')
+    if page is None:
+        return 0, 0
+
+    return page.find(r'<div id="mw-content-text"'), page.find(r'<div id="catlinks"')
 
 
 def extract_links(page: str, begin: int, end: int) -> list:
@@ -39,40 +39,13 @@ def extract_links(page: str, begin: int, end: int) -> list:
     задающего позицию содержимого статьи на странице и возвращает все имеющиеся
     ссылки на другие вики-страницы без повторений и с учётом регистра.
     """
-    links = re.findall(r'"(/wiki/.*?)"', page)
-    for i in enumerate(links):
-        if i[1].startswith('http'):
-            continue
-        links[i[0]] = 'http://ru.wikipedia.org' + links[i[0]]
+    links = set(re.findall(r'"/wiki/([^\.]*?)"', page[begin:end]))
+    links = list(links)
+    for index, link in enumerate(links):
+        links[index] = unquote(link)
 
     return links
-
-
-def get_page_title(page: str) -> str:
-    """Возращает значение поля title html страницы"""
-    return str(*re.findall(r"<title>(.*)</title>", page))
-
-
-def find_path(start_link: str, finish_link: str, path: list, counter: int):
-    if counter > 3:
-        return
-    print(counter)
-    print(start_link)
-    path.append(start_link)
-    if finish_link in start_link:
-        print('DONE')
-        return path
-    page = get_content(start_link)
-    if page is None:
-        return
-    links = extract_links(page, *extract_content(page))
-    counter += 1
-    for i in links:
-        if i in path:
-            continue
-        find_path(i, finish_link, path, counter)
         
-
 
 def find_chain(start: str, finish: str) -> list:
     """
@@ -81,18 +54,36 @@ def find_chain(start: str, finish: str) -> list:
     Первым элементом результата должен быть start, последним — finish.
     Если построить переходы невозможно, возвращается None.
     """
-    pass
+    path = [start]
+    current_name = start
+    while True:
+        print(current_name)
+        path.append(current_name)
+        if current_name == finish:
+            break
+        page = get_content(current_name)
+        links = extract_links(page, *extract_content(page))
+        if finish in links:
+            path.append(finish)
+            break
+        for link in links:
+            if link in path:
+                continue
+            current_name = link
+            break
+
+    return path
 
 
 def main():
-    page = get_content('Аристотель')
+    page = get_content('Архимед')
     begin, end = extract_content(page)
     links = extract_links(page, begin, end)
     print(len(links))
-    start_link = 'http://ru.wikipedia.org/wiki/' + quote('Аристотель')
-    finish_link = 'http://ru.wikipedia.org/wiki/' + quote('Философия')
-
-   # print(find_path(start_link, finish_link, [], 0))
+    print(find_chain('Архимед', 'Философия'))
+    # start_link = 'Философия_науки'
+    # finish_link = 'История_философии'
+    # print(find_path(start_link, finish_link, [], 0))
 
 
 if __name__ == '__main__':
